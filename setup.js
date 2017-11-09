797d37bb097bb20d35ebeffaec7c2b824e2796a1
Nathan Yan <s-yann@bsd405.org>
2017-11-08 23:24:00 -0800

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,11 @@
+var dl = deeplearn;
+
+// Allows for GPU computation
+var math = new dl.NDArrayMathGPU();
+
+// Easier to remember names (?)
+var Scalar = dl.Scalar;
+var Vector = dl.Array1D;
+var Matrix = dl.Array2D;
+var Tensor3 = dl.Array3D;
+var Tensor4 = dl.Array4D;
--- source.js
+++ source.js
@@ -1,25 +1,33 @@
 try{
     // WRITE CODE HERE
+	const g = new dl.Graph();
+	
+	const input = g.placeholder('inp', [1]);
+	const target = g.placeholder('target', [1]);
+	
+	const multiplier = g.variable("weight", Scalar.randNormal([1]));
+	
+	var result = g.multiply(input, multiplier);
+	
+	// compute square distance loss 
+	var square_distance = g.square(g.subtract(result, target), 2);
 
-    var dl = deeplearn;
-
-    var math = new dl.NDArrayMathGPU();
-    var a = dl.Array4D.new([2, 2, 5, 2], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-    var b = dl.Scalar.new(2);
-
-    var diff = math.sub(a, b);
-
-    print(diff, "diff");
-    print(a, 'a');
-    print(a, 'a');
-
-    for (i = 0; i < 10; i++){
-        print("11/" + i + " = " + 11/i);
-    }
-
-    print("hello");
-    print("text");
-    print('test')
+	const learning_rate = 0.001;
+	const batch_size = 3;
+	
+	const session = new dl.Session(g, math);
+	const optimizer = new SGDOptimizer(learning_rate);
+	
+	for (i = 0; i < 100; i++){
+		var val = Math.rand();
+		var input = Matrix.new([1, 1], [val]);
+		var target = Matrix.new([1, 1], [val * 2]);
+		
+		const cost = session.train(square_distance, {'inp' : input, 'target' : target}, 1, optimizer, CostReduction.MEAN);
+		
+		print(cost);
+	}
+	
 }catch(e){
     document.getElementById('output').innerHTML += "<span style = 'color: b71c1c'>" + e.toString() + '<br><br/></span>';
-}s
+}
